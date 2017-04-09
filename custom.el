b63d7c5f4e009629f71d376e2c0f02f1f9ebd531
Sean O'Reilly <sean@Seans-MacBook-Pro.local>
2017-04-09 21:55:41 +0200

    Added agenda files
    Added journal and ledger
    
    Added octave mode file associations
    
    Added org-brain. Loading all files in dir for agenda mode.
    
    Moved org brain files to home dir.
    
    Using gandalf theme
    
    Added ignore files
    
    Added feeds and dracula theme
    
    Updated feeds and org dir
    
    Set org dir
    
    Removed stuff
    
    Added GPG configuration
    
    Moved elfeed links to org file

--- custom.el
+++ custom.el
@@ -3,9 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "b8929cff63ffc759e436b0f0575d15a8ad7658932f4b2c99415f3dde09b32e97" "25c242b3c808f38b0389879b9cba325fb1fa81a0a5e61ac7cae8da9a32e2811b" "146061a7ceea4ccc75d975a3bb41432382f656c50b9989c7dc1a7bb6952f6eb4" "a61109d38200252de49997a49d84045c726fa8d0f4dd637fce0b8affaa5c8620" "3de3f36a398d2c8a4796360bfce1fa515292e9f76b655bb9a377289a6a80a132" "4af6fad34321a1ce23d8ab3486c662de122e8c6c1de97baed3aa4c10fe55e060" "a85e40c7d2df4a5e993742929dfd903899b66a667547f740872797198778d7b5" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+ '(global-hl-line-mode t)
+ '(helm-mode t)
+ '(ledger-reports
+   (quote
+    (("balance" "ledger ")
+     ("bal" "%(binary) -f %(ledger-file) bal")
+     ("reg" "%(binary) -f %(ledger-file) reg")
+     ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+     ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
  '(package-selected-packages
    (quote
-    (robe rvm monokai-theme fiplr flx-ido projectile-rails web-mode company use-package magit exec-path-from-shell))))
+    (solarized-theme csv-mode org-cliplink yaml-mode dracula helm-google indium js2-mode smartparens solidity-mode elfeed rubocop gandalf-theme darcula-theme base16-theme markdown-mode pdf-tools yasnippet org-brain flycheck avy helm-ag ag helm-projectile expand-region helm matlab-mode ledger-mode dtrt-indent clang-format org-journal dracula-theme restclient robe rvm monokai-theme fiplr flx-ido projectile-rails web-mode company use-package magit exec-path-from-shell)))
+ '(projectile-mode t nil (projectile))
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
